f526d41aee0369256c96c739f9ea4ebb0ae75f61
SOHNJEONGHYEON <sohnjunior@naver.com>
2019-07-29 03:36:16 +0900

    heroku7

--- YANAEDO/settings.py
+++ YANAEDO/settings.py
@@ -117,6 +117,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
